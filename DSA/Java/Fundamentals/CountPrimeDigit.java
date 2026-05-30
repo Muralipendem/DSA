78869f509dc3d0cce39731a85f45e710a3a00f8d
MuraliPendem <pendemmurali63@gmail.com>
2026-05-30 11:18:58 +0530

    CountPrimeDigit code opimized

--- DSA/Java/Fundamentals/CountPrimeDigit.java
+++ DSA/Java/Fundamentals/CountPrimeDigit.java
@@ -2,17 +2,12 @@
 public class CountPrimeDigit{
     public static void main(String args[]){
         int count=0;
-        int n=4680;
+        int n=23067;
         int rem;
         while(n!=0){
             rem=n%10;
             n=n/10;
-            boolean flag=true;
-            if(rem==1 || rem==0) flag=false;
-            else for(int i=2;i*i<=rem;i++){
-                if(rem%i==0) flag=false;break;
-            }
-            if(flag) count++;
+            if(rem==2 || rem==3 || rem==5 || rem==7) count++;
         }
         System.out.println(count);
     }
