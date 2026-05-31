453ab12e01a2d5dadcb5ce72a138c92e83ff650f
MuraliPendem <pendemmurali63@gmail.com>
2026-05-31 22:44:47 +0530

    Change update on spiral file

--- DSA/Java/Arrays/spiralArray.java
+++ DSA/Java/Arrays/spiralArray.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class spiralArray{
     public static void main(String[] args){
         int[][] matrix = {
@@ -5,29 +6,31 @@ public class spiralArray{
             {5, 6, 7, 8},
             {9, 10, 11, 12}
         };
+        ArrayList<Integer> spiral = new ArrayList<>();
         int left=0, right=matrix[0].length-1, top=0, bottom=matrix.length-1;
         while(left<=right && top<=bottom){
             for(int i=left; i<=right; i++){
-                System.out.print(matrix[top][i]+" ");
+                spiral.add(matrix[top][i]);
             }
             top++;
             for(int i=top; i<=bottom; i++){
-                System.out.print(matrix[i][right]+" ");
+                spiral.add(matrix[i][right]);
             }
             right--;
             if(top<=bottom){
                 for(int i=right; i>=left; i--){
-                    System.out.print(matrix[bottom][i]+" ");
+                    spiral.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
             if(left<=right){
                 for(int i=bottom; i>=top; i--){
-                    System.out.print(matrix[i][left]+" ");
+                    spiral.add(matrix[i][left]);
                 }
                 left++;
             }
         }
+        System.out.println(spiral);
 
     }
 }
