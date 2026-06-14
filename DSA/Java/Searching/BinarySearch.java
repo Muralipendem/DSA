047dbc964e504625e4dced2ed6285ef7baf0cd1c
MuraliPendem <pendemmurali63@gmail.com>
2026-06-14 19:19:18 +0530

    Add searching folder

---
+++ DSA/Java/Searching/BinarySearch.java
@@ -0,0 +1,21 @@
+package DSA.Java.Searching;
+
+public class BinarySearch {
+    public static void main(String args[]){
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9,left=0,right=nums.length-1;
+        int res=-1;
+        while(left<=right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid]==target){
+                res=mid;
+                break;
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        System.out.println(res);
+    }
+}
